a947891a58a0a0735eb5e4bb7886c0edb108e675
Jaymes Phimmachack <phimmachack.jaymes@gmail.com>
2022-11-16 13:33:41 -0500

    Connected Mongo db

--- backend/server.ts
+++ backend/server.ts
@@ -2,7 +2,11 @@ import * as express from 'express'
 import { PORT } from "./utils/config";
 import projectRoutes from './routes/projectRoutes'
 import { errorHandler } from "./middleware/errorMiddleware";
+import { connectDB } from "./database/db";
+import * as Colors from 'colors.ts'
+Colors.colors('', '')
 
+connectDB()
 const app = express()
 app.use(express.json())
 
--- backend/utils/config.ts
+++ backend/utils/config.ts
@@ -2,3 +2,4 @@ require('dotenv').config()
 
 export const PORT = process.env.PORT || 8000
 export const NODE_ENV = process.env.NODE_ENV || 'developement'
+export const MONGO_URI = process.env.MONGO_URI || null
